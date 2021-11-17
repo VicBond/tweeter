eee716a324eba0a99cb673764b74812939bab49c
Victor Bondarenko <nirasimha@gmail.com>
2021-11-17 22:17:27 +0000

    fetching GET data from the server with AJAX

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,27 +1,27 @@
 /* eslint-disable no-undef */
 const tweetData = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
+  // {
+  //   "user": {
+  //     "name": "Newton",
+  //     "avatars": "https://i.imgur.com/73hZDYK.png"
+  //     ,
+  //     "handle": "@SirIsaac"
+  //   },
+  //   "content": {
+  //     "text": "If I have seen further it is by standing on the shoulders of giants"
+  //   },
+  //   "created_at": 1461116232227
+  // },
+  // {
+  //   "user": {
+  //     "name": "Descartes",
+  //     "avatars": "https://i.imgur.com/nlhLi3I.png",
+  //     "handle": "@rd" },
+  //   "content": {
+  //     "text": "Je pense , donc je suis"
+  //   },
+  //   "created_at": 1461113959088
+  // }
 ];
 
 
@@ -58,11 +58,25 @@ $(document).ready(function() {
       method: "POST",
       url: "/tweets",
       data: str
-    }).then(function() {
-      console.log("POST request sent");
-    });
+    })
+      .then(function() {
+        console.log("POST request sent");
+      });
   });
+  const loadTweets = function() {
+    $.ajax({
+      method: "GET",
+      url: "/tweets",
+      data: JSON
+    })
+      .then(function(data) {
+        $("#tweets-container").empty();
+        renderTweets(data);
+      });
+  };
+  loadTweets();
 });
+
 
 const renderTweets = function(tweets) {
   for (let tweet of tweets) {
